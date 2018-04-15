558c4568bcbe86aba7c7dfa07157b834c4571fcb
vftiago <vf.tiago@gmail.com>
2018-04-15 18:05:10 +0100

    feat(api): consume data from mongodb

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,46 +2,46 @@ import axios from "axios";
 import DEFAULTS from "./../defaults";
 
 const instance = axios.create({
-	baseURL: "http://localhost:4000",
-	timeout: 5000
+    baseURL: "http://localhost:5000/api",
+    timeout: 5000
 });
 
 const api = {
-	fetchCount: async (lang: string = DEFAULTS.lang) => {
-		try {
-			const response = await instance.get("counts", {
-				params: { lang }
-			});
-			return response.data[0].count;
-		} catch (error) {
-			console.error(error);
-		}
-	},
+    fetchCount: async (lang: string = DEFAULTS.lang) => {
+        try {
+            const response = await instance.get("counts", {
+                params: { lang }
+            });
+            return response.data;
+        } catch (error) {
+            console.error(error);
+        }
+    },
 
-	fetchItem: async (lang: string = DEFAULTS.lang, id: number) => {
-		try {
-			const response = await instance.get("proverbs", {
-				params: { lang, id }
-			});
-			return response.data;
-		} catch (error) {
-			console.error(error);
-		}
-	},
+    fetchItem: async (lang: string = DEFAULTS.lang, id: number) => {
+        try {
+            const response = await instance.get("proverbs", {
+                params: { lang, id }
+            });
+            return response.data;
+        } catch (error) {
+            console.error(error);
+        }
+    },
 
-	fetchList: async (
-		lang: string = DEFAULTS.lang,
-		_limit: number = DEFAULTS.pageSize
-	) => {
-		try {
-			const response = await instance.get("proverbs", {
-				params: { lang, _limit }
-			});
-			return response.data;
-		} catch (error) {
-			console.error(error);
-		}
-	}
+    fetchList: async (
+        lang: string = DEFAULTS.lang,
+        _limit: number = DEFAULTS.pageSize
+    ) => {
+        try {
+            const response = await instance.get("proverbs", {
+                params: { lang, _limit }
+            });
+            return response.data;
+        } catch (error) {
+            console.error(error);
+        }
+    }
 };
 
 export default api;
