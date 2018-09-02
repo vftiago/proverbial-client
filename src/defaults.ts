7b9c0cf89f3f5404579f7f5b2c579716d7d60a70
vftiago <vf.tiago@gmail.com>
2018-09-02 20:47:31 +0100

    fix(core): remove the completely useless react-google-login

--- src/defaults.ts
+++ src/defaults.ts
@@ -12,10 +12,4 @@ const COLORS = {
     iconFill: "#333"
 };
 
-const CREDENTIALS = {
-    GOOGLE_CLIENT_ID:
-        "130599439121-7hmvog2onl63mak3nbdr7f9qj3m9vo7s.apps.googleusercontent.com",
-    GOOGLE_CLIENT_SECRET: "5sADx6fZvGar6VWFoFo5Yf9G"
-};
-
-export { DEFAULTS, COLORS, CREDENTIALS };
+export { DEFAULTS, COLORS };
