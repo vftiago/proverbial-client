f185ac42b5d5156826a492947888ca0ea72619d5
vftiago <vf.tiago@gmail.com>
2018-09-08 16:35:23 +0100

    fix(core): attempting to deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,37 @@
+const HtmlWebpackPlugin = require( 'html-webpack-plugin' );
+
 module.exports = {
-	entry: "./src/index.tsx",
-	output: {
-		filename: "bundle.js",
-		path: __dirname + "/dist"
-	},
-	// enable source maps for debugging
-	devtool: "source-map",
-	resolve: {
-		extensions: [".ts", ".tsx", ".js", ".json"]
-	},
-	module: {
-		rules: [
-			{ test: /\.tsx?$/, loader: "awesome-typescript-loader" },
-			{ enforce: "pre", test: /\.js?$/, loader: "source-map-loader" }
-		]
-	},
-	externals: {
-		// avoid bundling all our dependencies, which allows browsers to cache libraries between builds
-		react: "React",
-		"react-dom": "ReactDOM"
-	}
+    entry: "./src/index.tsx",
+    output: {
+        filename: "bundle.js",
+        path: __dirname + "/dist"
+    },
+    // enable source maps for debugging
+    devtool: "source-map",
+    resolve: {
+        extensions: [ ".ts", ".tsx", ".js", ".json" ]
+    },
+    module: {
+        rules: [ {
+                test: /\.tsx?$/,
+                loader: "ts-loader"
+            },
+            {
+                enforce: "pre",
+                test: /\.js?$/,
+                loader: "source-map-loader"
+            }
+        ]
+    },
+    externals: {
+        // avoid bundling all our dependencies, which allows browsers to cache libraries between builds
+        react: "React",
+        "react-dom": "ReactDOM"
+    },
+    plugins: [
+        new HtmlWebpackPlugin( {
+            title: 'Proverbial App',
+            filename: './index.html'
+        } )
+    ]
 };
