a8cd151e3868c18ee4732ce606dabe7bbfa43e76
vftiago <vf.tiago@gmail.com>
2018-09-08 19:12:46 +0100

    fix(depndencies): replace all previous options with harp

--- src/server.ts
+++
@@ -1,8 +0,0 @@
-import * as connect from "connect";
-import * as serveStatic from "serve-static";
-
-connect()
-    .use(serveStatic(__dirname))
-    .listen(8080, function() {
-        console.log("Server running on 8080");
-    });
