ee383c8bf01e5380774ceca1a581ed75fde12e58
vftiago <vf.tiago@gmail.com>
2018-09-08 18:58:13 +0100

    feat(core): add Procfile and http-server

---
+++ src/server.ts
@@ -0,0 +1,8 @@
+import * as connect from "connect";
+import * as serveStatic from "serve-static";
+
+connect()
+    .use(serveStatic(__dirname))
+    .listen(8080, function() {
+        console.log("Server running on 8080");
+    });
